aaa2dc6a59e278353baf3a98da630b204c4a9da8
ahmedsindbad <ahmedkamal19855@gmail.com>
2018-02-02 23:06:55 +0200

    Fix search engine for arabic encode utf-8

--- Haseb/Search/classSearch.py
+++ Haseb/Search/classSearch.py
@@ -1,19 +1,25 @@
+from __future__ import unicode_literals
+from django.utils.encoding import python_2_unicode_compatible
+
 import sqlite3
 import os
 
 
 #database phyzical path ....
+@python_2_unicode_compatible
+class EXE:
+    def exe(value):
+        conn = sqlite3.connect('C://test//myenv//Haseb//db.sqlite3')
+        c = conn.cursor()
+        c.execute("select text from Search_verse where text like '%"+value+"%';", )
+        result = c.fetchall()
+        #print(unicode(result, "utf-8"))
+        conn.commit()
+        conn.close()
+        return result
 
+    def __str__(self):
+        return self
 
-def exe(want):
-    conn = sqlite3.connect('C://test//myenv//Haseb//db.sqlite3')
-    c = conn.cursor()
-    c.execute("select text from Search_verse where text like '%"+want+"%';", )
-    result = str(c.fetchall()).encode(encoding='utf-8')
-    #print(unicode(result, "utf-8"))
-    conn.commit()
-    conn.close()
-    return result
-
-
-
+    #def __init__(self, name):
+        #self.name = name
--- Haseb/Search/views.py
+++ Haseb/Search/views.py
@@ -3,9 +3,10 @@ from __future__ import unicode_literals
 from django.views.generic.base import TemplateView
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .forms import VerseForm
-from .classSearch import exe
+#from django.utils.encoding import python_2_unicode_compatible
 
+from .forms import VerseForm
+import sqlite3
 
 # Create your views here.
 class HomeView(TemplateView):
@@ -23,15 +24,20 @@ class TestPageView(TemplateView):
     template_name = 'testform.htm'
 
 
-
+#@python_2_unicode_compatible
 def varse(request):
     form = VerseForm(request.POST or None)
-
     if form.is_valid():
         for key, value in form.cleaned_data.iteritems():
             print key, value
-        print (exe(value))
-        d = exe(value)
+        conn = sqlite3.connect('C://test//myenv//Haseb//db.sqlite3')
+        c = conn.cursor()
+        c.execute("select text from Search_verse where text like '%"+value+"%';", )
+        result = list(c)
+        conn.commit()
+        for w in result:
+            print(w[0])
+        conn.close()
     context = {
         "form": form,
     }
